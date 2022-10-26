efaefc5ef0682b3ee646be6dd197425e0db86783
yevheniikravchenko <eugenykravchenko@gmail.com>
2022-10-26 09:52:48 +0200

    Hometask 1-3

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
+import {MessagePropsType} from "../message/Message";
+
+// создать тип вместо any и отобразить приходящие данные
+const FriendMessage: React.FC<MessagePropsType> = (props) => {
+    return (
+        <div
+            id={'hw1-friend-message-' + props.message.id}
+            className={s.friendMessage}
+        >
+            <div className={s.friendImageAndText}>
+                <img
+                    id={'hw1-friend-avatar-' + props.message.id}
+                    src={props.message.user.avatar}
+                />
+                <div className={s.friendText}>
+                    <div
+                        id={'hw1-friend-name-' + props.message.id}
+                        className={s.friendName}
+                    >
+                        <div>{props.message.user.name}</div>
+                    </div>
+                    <pre
+                        id={'hw1-friend-text-' + props.message.id}
+                        className={s.friendMessageText}
+                    >
+                        <div>{props.message.message.text}</div>
+                    </pre>
+                </div>
+            </div>
+            <div
+                id={'hw1-friend-time-' + props.message.id}
+                className={s.friendTime}
+            >
+                <div>{props.message.message.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default FriendMessage
