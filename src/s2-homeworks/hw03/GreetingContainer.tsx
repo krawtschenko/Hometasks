21f74dd99d4b94098e26a9872014c50bf1a9e0ac
yevheniikravchenko <eugenekravchenko.com>
2022-10-26 22:35:30 +0200

    Home-task 3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -17,7 +17,7 @@ export const pureAddUser = (name: string, setError: (error: string) => void, set
     }
 }
 
-const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
     if (name.trim() === '') {
         setError('Ошибка! Введите имя!')
     }
