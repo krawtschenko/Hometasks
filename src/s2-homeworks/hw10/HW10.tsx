4acee921ef43bbb62868fc697699327d28a4af9e
yevheniikravchenko <eugenykravchenko@gmail.com>
2022-12-16 09:40:03 +0100

    HW10 + CSS

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -25,7 +25,7 @@ const HW10 = () => {
         dispatch(loadingAC(true))
         setTimeout(() => {
             dispatch(loadingAC(false))
-        }, 10000)
+        }, 1500)
     };
 
     return (
